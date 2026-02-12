7ffd657d3ffcfc81a4b6678d9e9122ee21ce8f30
Luis <simosa37@gmail.com>
2026-02-12 10:38:11 -0300

    feat: add tranche 1 navigation content types

---
+++ src/api/main-navigation-dropdown/controllers/main-navigation-dropdown.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * main-navigation-dropdown controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::main-navigation-dropdown.main-navigation-dropdown');
