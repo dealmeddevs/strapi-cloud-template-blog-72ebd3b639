9c994c21af46e4571609b393ecd25c9e7d6c41cf
Luis <simosa37@gmail.com>
2026-02-12 15:13:49 -0300

    feat: add tranche 2 page-shell content types

---
+++ src/api/who-we-serve-page/routes/who-we-serve-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * who-we-serve-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::who-we-serve-page.who-we-serve-page');
