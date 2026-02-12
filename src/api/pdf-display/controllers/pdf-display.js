01d666cf57eea92c815e8f015c70a85d96a0997d
Luis <simosa37@gmail.com>
2026-02-12 18:59:40 -0300

    feat: add tranche 3 contact and document content types

---
+++ src/api/pdf-display/controllers/pdf-display.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pdf-display controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pdf-display.pdf-display');
---
+++ src/api/pdf-display/routes/pdf-display.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pdf-display router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pdf-display.pdf-display');
