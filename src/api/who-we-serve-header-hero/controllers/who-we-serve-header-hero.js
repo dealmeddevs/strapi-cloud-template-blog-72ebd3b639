379997d694fb2b18fc9f1cb8d3256708a26898c5
Luis <simosa37@gmail.com>
2026-02-13 14:51:50 -0300

    Revert "removed images and added final content types"
    This reverts commit 973bb536d3b6203ed3aae880837e401b516849d1.

--- src/api/who-we-serve-header-hero/controllers/who-we-serve-header-hero.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * who-we-serve-header-hero controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::who-we-serve-header-hero.who-we-serve-header-hero');
--- src/api/who-we-servemarkets-served-servicesservice/controllers/who-we-servemarkets-served-servicesservice.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * who-we-servemarkets-served-servicesservice controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::who-we-servemarkets-served-servicesservice.who-we-servemarkets-served-servicesservice');
