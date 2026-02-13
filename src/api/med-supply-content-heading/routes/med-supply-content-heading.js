077c7c81753bf7b29abeb44a9e75eb312817a46d
Luis <simosa37@gmail.com>
2026-02-13 11:19:12 -0300

    feat: add tranche 7B med supply content types

---
+++ src/api/med-supply-content-heading/routes/med-supply-content-heading.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * med-supply-content-heading router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::med-supply-content-heading.med-supply-content-heading');
