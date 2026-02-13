1404d3dd3c4b9d7f2587eb7828cf98c77e88ff9c
Luis <simosa37@gmail.com>
2026-02-13 12:33:35 -0300

    feat: add tranche 7C1 who-we-serve markets served type

---
+++ src/api/who-we-serve-markets-served-service/controllers/who-we-serve-markets-served-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * who-we-serve-markets-served-service controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::who-we-serve-markets-served-service.who-we-serve-markets-served-service');
---
+++ src/api/who-we-serve-markets-served-service/services/who-we-serve-markets-served-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * who-we-serve-markets-served-service service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::who-we-serve-markets-served-service.who-we-serve-markets-served-service');
