9d1cbd686b6359b1bb7091442377e80b802e58c8
Luis <simosa37@gmail.com>
2026-02-13 09:51:03 -0300

    feat: add tranche 6 five low-coupling content types

---
+++ src/api/home-we-serve/controllers/home-we-serve.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-we-serve controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-we-serve.home-we-serve');
---
+++ src/api/home-we-serve/routes/home-we-serve.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-we-serve router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-we-serve.home-we-serve');
